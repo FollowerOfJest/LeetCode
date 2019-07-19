63fe44ec2dc44dd7666a99616d5db949088ab225
wzm <not_enaf@qq.com>
2019-07-19 17:21:35 +0800

    add 39 40 total 39

--- _39_Combination_Sum.java
+++ _39_Combination_Sum.java
@@ -8,51 +8,32 @@ import java.util.List;
  * Created by 54359 on 2017/6/29.
  */
 public class _39_Combination_Sum {
-    //自己写的错了，这是抄的
+    //都是正数
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        //先排序
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         getResult(result, new ArrayList<Integer>(), candidates, target, 0);
 
         return result;
     }
-
+    //递归
     private void getResult(List<List<Integer>> result, List<Integer> temp, int candidates[], int target, int start){
-        if(target > 0){
+        if(target > 0){ //只有剩余的值>=0才有意义，小于0说明不需要进一步判断了，因为数组已经排序了
+            //这里基本就是考虑了所有的可能，从start开始一个一个试，+当前值 不+当前值，两种情况分别递归
             for(int i = start; i < candidates.length && target >= candidates[i]; i++){
                 temp.add(candidates[i]);
+                //因为可以无限使用每个数字，所以start还是i
                 getResult(result, temp, candidates, target - candidates[i], i);
                 temp.remove(temp.size() - 1);
             }
         }
         else if(target == 0 ){
+            //这里记得要用这种方式，因为temp是引用对象，直接放的话是引用不行的
             result.add(new ArrayList<>(temp));
         }
     }
 
-    /*阳
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-		List<List<Integer>> data = new ArrayList<>();
-		helper(data, new ArrayList<Integer>(), candidates, target, 0);
-		return data;
-    }
-    public void helper(List<List<Integer>> data, List<Integer> temp, int[] candidates, int target, int index) {
-		for (int i = index; i < candidates.length; ++i) {
-			if (target > candidates[i]) {
-				List<Integer> next = new ArrayList<>(temp);
-				next.add(candidates[i]);
-				helper(data, next, candidates, target - candidates[i], i);
-			} else if (target == candidates[i]) {
-				List<Integer> next = new ArrayList<>(temp);
-				next.add(candidates[i]);
-				data.add(next);
-				break;
-			} else {
-				break;
-			}
-		}
-	}
-    */
+    
 
 }
--- _40_Combination_Sum_II.java
+++ _40_Combination_Sum_II.java
@@ -8,49 +8,27 @@ import java.util.List;
  * Created by 54359 on 2017/6/30.
  */
 public class _40_Combination_Sum_II {
-    /*/这是仿照_39写的，但是这种有重复了，应该更改
+
+    // 代码与39相似，参考15中的重复跳过。
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
         recursion(result, new ArrayList<>(), candidates, target, 0);
-        return  result;
+        return result;
     }
 
-    private void recursion(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start){
-        if(target > 0){
-            for(int i = start; i < candidates.length && target >= candidates[i]; i++){
+    private void recursion(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start) {
+        if (target > 0) {
+            for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
+                //这里的跳过规则同15题中的跳跃规则
+                if (i > start && candidates[i] == candidates[i - 1])
+                    continue; // 这里负责跳过重复的
                 cur.add(candidates[i]);
-                recursion(result, cur, candidates, target - candidates[i], i+1);
+                recursion(result, cur, candidates, target - candidates[i], i + 1); // 因为不能重复找数字了，所以得去下一个位置
                 cur.remove(cur.size() - 1);
             }
-        }
-        else if(target == 0 ){
-            if(!result.contains(cur)){
-                result.add(new ArrayList<>(cur));
-            }
-        }
-    }
-    */
-
-    //代码与39相似，参考15中的重复跳过。
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> result = new ArrayList<>();
-        recursion(result, new ArrayList<>(), candidates, target, 0);
-        return  result;
-    }
-
-    private void recursion(List<List<Integer>> result, List<Integer> cur, int candidates[], int target, int start){
-        if(target > 0){
-            for(int i = start; i < candidates.length && target >= candidates[i]; i++){
-                if (i > start && candidates[i] == candidates[i-1]) continue;    //这里负责跳过重复的
-                    cur.add(candidates[i]);
-                    recursion(result, cur, candidates, target - candidates[i], i+1);    //因为不能重复找数字了，所以得去下一个位置
-                    cur.remove(cur.size() - 1);
-            }
-        }
-        else if(target == 0 ){
-                result.add(new ArrayList<>(cur));
+        } else if (target == 0) {
+            result.add(new ArrayList<>(cur));
         }
     }
 }
