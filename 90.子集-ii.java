a607e6edea9653c165d9ce3648176b94fcd76803
wzm <not_enaf@qq.com>
2019-07-23 21:37:22 +0800

    total 68

--- 90.子集-ii.java
+++ 90.子集-ii.java
@@ -4,7 +4,36 @@
  * [90] 子集 II
  */
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    //位运算
+    public List<List<Integer>> subsetsWithDup(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> lists = new ArrayList<>();
+        int subsetNum = 1<<num.length;
+        for(int i=0;i<subsetNum;i++){
+            List<Integer> list = new ArrayList<>();
+            boolean illegal=false;
+            for(int j=0;j<num.length;j++){
+                //当前位是 1
+                if((i>>j&1)==1){
+                    //当前是重复数字，并且前一位是 0，跳过这种情况
+                    if(j>0&&num[j]==num[j-1]&&(i>>(j-1)&1)==0){
+                        illegal=true;
+                        break;
+                    }else{
+                        list.add(num[j]);
+                    }
+                }
+            }
+            if(!illegal){
+                lists.add(list); 
+            }
+    
+        }
+        return lists;
+    }
+    
+
+    public List<List<Integer>> subsetsWithDup_1(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
         backtrack(list, new ArrayList<>(), nums, 0);
