0468f3d2d5836588d1f58b3d571a32b62d5906e2
wzm <not_enaf@qq.com>
2019-07-30 09:27:57 +0800

    total 87

--- 102.二叉树的层次遍历.java
+++ 102.二叉树的层次遍历.java
@@ -14,7 +14,23 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            while (size != 0) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+                size--;
+            }
+            result.add(temp);
+        }
+        return result;
     }
 }
 
